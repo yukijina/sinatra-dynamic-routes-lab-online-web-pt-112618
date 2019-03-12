d9ed4967e49ed6382519258ce8e7cd5a87231199
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-03-12 03:38:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -14,11 +14,32 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @num = params[:number].to_i
+    @num =  params[:number].to_i 
     @phrase = params[:phrase]
-
-    @num.times {|n| @phrase}
-
+    
+    result = []
+    @num.times {|n| result << @phrase}
+    "#{result.join(" ")}"
   end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1] + ' ' + params[:word2] + ' ' + params[:word3] + ' ' + params[:word4] + ' ' + params[:word5]}."
+  end
+  
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @n1 = params[:number1].to_i
+    @n2 = params[:number2].to_i
+    
+    if @operation == "add"
+      "#{@n1 + @n2}"
+    elsif @operation == "subtract"
+      "#{@n1 - @n2}"
+    elsif @operation == "multiply"
+      "#{@n1 * @n2}"
+    elsif @operation == "divide"
+     "#{@n1 / @n2}"
+    end 
+  end 
 
 end
